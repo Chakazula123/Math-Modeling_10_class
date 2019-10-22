ebdffb8b19d4eedeec707191424b5ab7c1e02bfb
Chakazula123 <56309230+Chakazula123@users.noreply.github.com>
2019-10-22 17:48:42 +0200

    я зэ бэст челик

---
+++ Laba 2.py
@@ -0,0 +1,6 @@
+print('--------Задание 2---------')
+import my_module as mc
+import numpy as np
+import math as mt
+v=((mc.g)*(mc.h)*(np.tan(mc.b))**2/(2*(np.cos(mc.a))**2*(1-(np.tan(mc.b))*(np.tan(mc.a)))))**0.5
+print(v)
